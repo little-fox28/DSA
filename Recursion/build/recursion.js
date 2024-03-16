e6f8f9e474212a59858108cd54f17dc96245b9c7
little-fox28 <tranthjnh88@gmail.com>
2024-03-16 15:13:19 +0700

    Done recursion!

---
+++ Recursion/build/recursion.js
@@ -0,0 +1,8 @@
+function fib(element) {
+    if (element < 2)
+        return 1;
+    return fib(element - 1) + fib(element - 2);
+}
+console.log(fib(1));
+console.log(fib(4));
+console.log(fib(6));
